4b98c64f18c603279fc9456d4cbdf9c34168bf8d
jpezninjo <joperez@dons.usfca.edu>
2017-01-03 10:08:15 -0800

    Added finished template

---
+++ testrun.py
@@ -0,0 +1,28 @@
+hero = [
+                ("https://blzgdapipro-a.akamaihd.net/game/heroes/small/0x02E0000000000065.png",
+                "George1", 1, 2, 3, 25,
+                15, 25, 35,
+                25, 35, 45,
+                35, 45, 55
+                ),
+                ("https://blzgdapipro-a.akamaihd.net/game/heroes/small/0x02E0000000000065.png",
+                "George2", 1, 2, 3, 25,
+                15, 25, 35,
+                25, 35, 45,
+                35, 45, 55
+                ),
+                ("https://blzgdapipro-a.akamaihd.net/game/heroes/small/0x02E0000000000065.png",
+                "George3", 1, 2, 3, 25,
+                15, 25, 35,
+                25, 35, 45,
+                35, 45, 55
+                ),   
+            ]
+hero = [
+("https://blzgdapipro-a.akamaihd.net/game/heroes/small/0x02E0000000000065.png","George3", 1, 2, 3, 25, 15, 25, 35, 25, 35, 45, 35, 45, 55),
+("https://blzgdapipro-a.akamaihd.net/game/heroes/small/0x02E0000000000065.png","George3", 1, 2, 3, 25, 15, 25, 35, 25, 35, 45, 35, 45, 55),
+("https://blzgdapipro-a.akamaihd.net/game/heroes/small/0x02E0000000000065.png","George3", 1, 2, 3, 25, 15, 25, 35, 25, 35, 45, 35, 45, 55)
+]
+
+for name, tim in hero.items():
+	print(name)	
